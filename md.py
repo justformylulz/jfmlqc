2de0152942dc28fd566f89fcfa5fd1261c1604c9
justformylulz <84847180+justformylulz@users.noreply.github.com>
2021-06-02 16:24:58 +0200

    Add files via upload

--- md.py
+++ md.py
@@ -1,48 +1,63 @@
+
+#-------------------------------------------------------------------#
+#                                                                   #
+#               MOLECULAR DYNAMICS SIMULATION                       #
+#               USED POTENTIAL: LENNARD-JONES-12-6                  #
+#               POSSIBLE ATOMS TO SIMULATE: Ar                      #
+#               ONLY GAS PHASE                                      #
+#               SIMULATION BOX LENGTH: 20 A                         #
+#       02.06.2021                          AUTHOR: @AZAD KIRSAN    #
+#-------------------------------------------------------------------#
+
+
+
+
+
+
+#----------------------------#
+#                            #
+#           START            #
+#                            #
+#----------------------------#
+
 import numpy as np
 import math
-#import mpl_toolkits.mplot3d.axes3d as p3
 from scipy.constants import k
 import time
 from itertools import product, combinations
 
-#-----init---------------------------------------------------------------------------------
+#-----------------------------------------------#
+#                                               #
+#           INITIALIZATION OF PARAMETERS        #
+#                                               #
+#-----------------------------------------------#
 
 class Simulation:
     def init (self):
-
-        #dimension of the simulation
         self.dim=3
-
         self.n_atom=1
-        
-        #set velocity and position-array to a zero-array with dimension of (number of atoms X dimension) 
-        #self.velocity=np.zeros((self.n_atom, self.dim))
-        #self.position=np.zeros((self.n_atom, self.dim))
-        
-        #just some parameters needed
         self.box_len=20 #in A
         self.dt=0.01 #in s
         self.steps=2000
         self.filename='filename'
         self.bc='HW'
-        self.epsilon=0.0103 #in eV
-        self.sigma=3.40 #in A
+        self.epsilon=0.0103 #in eV #
+        self.sigma=3.40 #in A      # LJ parameters
         self.m=39.948 #amu
 
 
-#-----init posi and velocity---------------------------------------------------------------------------------
+#-------------------------------------------------------#
+#                                                       #
+#           INITIALIZATION OF POSITION AND VELOCITY     #
+#                                                       #
+#-------------------------------------------------------#
 
-
-    #randomize each component of velocity through maxwell-boltzmann-distribution
-    #maxwell-boltzmann-distribution is a normal distribution of gas-particle velocities
-    #where the centre is at loc=0 and the (scale) std_dev is at sigma=sqrt(kT/m)
-    #this function sets the component of each velocity to a random value of that normal distribution
-    #luckily, numpy has a function that randomizes values considering the chosen distribution
     def init_velocity (self):
         #std_dev=np.sqrt(k*T/self.m)
-        #self.velocity=np.random.normal(loc=0, scale=std_dev, size=(self.n_atom, self.dim))*1e10 #in ~m/s *10^-13 deswegen *10^10 für ausgabe in angstrom
+        #self.velocity=np.random.normal(loc=0, scale=std_dev, size=(self.n_atom, self.dim))*1e10
+        
         R=np.random.rand(self.n_atom, self.dim)-0.5
-        self.velocity=R* (k*T/(self.m*1.602e-19))**0.5 *0
+        self.velocity=R* (k*T/(self.m*1.602e-19))**0.5 #in eV/amu
 
     def init_posi_from_file (self):
         #read number of atoms from the first line of the xyz file
@@ -61,8 +76,6 @@ class Simulation:
         self.y=self.position[:,1]
         self.z=self.position[:,2]
 
-
-
     
     def optimize_geo(self):
         for i in range(800):
@@ -72,7 +85,7 @@ class Simulation:
             r=(np.random.random_sample(size=3)-0.5)*2
             r_magnitude=np.linalg.norm(r)
             r_norm=r/r_magnitude
-            dr=0.05*self.sigma*r_norm
+            dr=0.075*self.sigma*r_norm
             
             self.position[particle] += dr
             pe_end=self.pe()
@@ -81,9 +94,11 @@ class Simulation:
 
 
 
-
-
-#-----energies---------------------------------------------------------------------------------
+#-------------------------------------------#
+#                                           #
+#           CALCULATION OF ENERGIES         #
+#                                           #
+#-------------------------------------------#
 
     def pe_interaction(self, particle_1, particle_2):
         r = self.get_min_dist(particle_1, particle_2)
@@ -109,7 +124,13 @@ class Simulation:
         return ekin_tot
 
 
-#-----force---------------------------------------------------------------------------------
+#-------------------------------------------#
+#                                           #
+#           CALCULATION OF FORCES           #
+#                                           #
+#-------------------------------------------#
+
+    #calculate the minimum distance between particle 1 and the nearest neighbor of particle 2 (can be an image)
 
     def get_min_dist(self, p1, p2):
         r_real=self.position[p1]-self.position[p2]
@@ -150,6 +171,7 @@ class Simulation:
     #the force is the derivative of the energy (potential)
     #here we use the LJ-12-6-potential, so we have to form the derivative after r
     #you can only calculate force in a direction by multiplying the force with the unit vector in that direction
+    
     def lj_interaction (self, particle_1, particle_2):
         r=self.get_min_dist(particle_1, particle_2) 
         r_magnitude=np.linalg.norm(r) 
@@ -159,8 +181,6 @@ class Simulation:
         return f_vector #return the force vector with the forces acting in each direction as components
 
     
-
-
 
     #determine all forces acting on a particle
     def lj_choose (self, p1):
@@ -172,16 +192,23 @@ class Simulation:
         return force #in eV/A
     
 
-#-----integrator---------------------------------------------------------------------------------
+#-----------------------------------------------#
+#                                               #
+#           INTEGRATOR AND BOUNDARIES           #
+#                                               #
+#-----------------------------------------------#
 
     def velocity_verlet (self):
         accel_0=np.zeros((self.n_atom, self.dim))
         energy=open('Energy.txt', 'w')
         trj=open('trj.xyz', 'w')
-#-----velocity verlet---------------------------------------------------------------------------------
+        
+
         for i in range(self.steps):
             self.position = self.position + 0.5*accel_0*(self.dt**2) +self.velocity*self.dt 
+            
             self.check_boundary()
+            
             self.x=self.position[:,0]
             self.y=self.position[:,1]
             self.z=self.position[:,2]
@@ -192,11 +219,16 @@ class Simulation:
                 zp=self.z[j]
                 trj.writelines(' '+'Ar'+' '+str('{0:.5f}'.format(xp))+' '+str('{0:.5f}'.format(yp))+' '+str('{0:.5f}'.format(zp))+'\n')
            
+            
             forces=np.array([self.lj_choose(p) for p in range(self.n_atom)])
+            
             accel_1=(forces/self.m) #in eV/A*amu
+            
             self.velocity = self.velocity + 0.5*(accel_0+accel_1)*self.dt
+            
             accel_0=accel_1
-#-----for energy plot---------------------------------------------------------------------------------
+            
+
             pe_tot=self.pe()
             ekin_tot=self.ekin()
             e_tot=pe_tot+ekin_tot
@@ -222,7 +254,12 @@ class Simulation:
 
 
 
-#choose the temperature and boundary conditions
+#-------------------------------#
+#                               #
+#           THE PROGRAM         #
+#                               #
+#-------------------------------#
+
 temperature=input("Choose the simulation temperature in Kelvin: ")
 T=float(temperature)
 j=False
@@ -232,7 +269,9 @@ while j is False:
         j=True
     else:
         print("Wrong input, please write either PBC or HW!")
-#-----start program-------------------------------------------------------------------------------------------------
+
+
+
 dyn=Simulation()
 dyn.init()
 dyn.bc=boundary
@@ -264,3 +303,8 @@ t_end=time.time()
 print("MD time=",t_end-t_start)
 
 
+#-------------------------------#
+#                               #
+#           END                 #
+#                               #
+#-------------------------------#
